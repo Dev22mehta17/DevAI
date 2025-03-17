45ebdb3e8c787319a36b0265a0930caba3e60a9b
dev mehta <mehtadev2004@gmail.com>
2025-03-17 18:16:13 +0530

    sdc

---
+++ dmai/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      hii this is dev
+      <br/>
+      <Button>Hello</Button>
+    </div>
+  );
+}
