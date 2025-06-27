1c46668cd7aaffd5103355cc61b0d53060072278
Dev Mehta <134418779+Dev22mehta17@users.noreply.github.com>
2025-06-27 16:13:14 +0530

    Update route.js

--- dmai/app/api/parse-pdf/route.js
+++ dmai/app/api/parse-pdf/route.js
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const { parsePdfBuffer } =      require('../../app/api/parse-pdf/parsePdf')
+const { parsePdfBuffer } = require('../../../app/api/parse-pdf/parsePdf');
+
 
 export const runtime = "nodejs";
 
